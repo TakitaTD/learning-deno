400dd2dbc38927a7021fe7c1ebb8d567a204892c
Tim Takita <tim@takitatd.dev>
2022-07-09 22:46:48 +1000

    e

--- index.ts
+++ index.ts
@@ -3,11 +3,9 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 
 const router = new Router();
-router.get("/init", (ctx) => {
-  console.log(ctx.ip);
+router.get("/", (ctx) => {
   ctx.response.body = "Hello";
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
-
 await app.listen({ port: 3000 });
